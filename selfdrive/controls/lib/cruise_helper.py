0c8bee048a84c2da82ee335738ffb199baf3d35f
ajouatom <ajouatom@gmail.com>
2023-05-25 07:01:12 +0900

    cxx: accel cruise off 수정.

--- selfdrive/controls/lib/cruise_helper.py
+++ selfdrive/controls/lib/cruise_helper.py
@@ -457,7 +457,7 @@ class CruiseHelper:
             v_cruise_kph = self.autoSyncCruiseSpeedMax
           v_cruise_kph_backup = v_cruise_kph
       # 앞차를 추월하기 위해 가속한경우, 앞차와의 거리가 감속가능한 거리가 아닌경우 크루즈OFF: 급격한 감속충격을 막기 위해.. (시험해야함)
-      elif 0 < self.dRel < CS.vEgo ** 2 / (2.5*2):
+      elif 0 < self.dRel < CS.vEgo * 0.9: # 급정거 t_follow 를 0.9로 가정..
         longActiveUser = -2
       #  6. 크루즈속도보다 높을때: 크루즈속도 현재속도셋 : autoSyncCruiseSpeedMax까지
       elif self.v_ego_kph > v_cruise_kph and self.autoSyncCruiseSpeedMax > self.autoResumeFromGasSpeed:
