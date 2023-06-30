fed683e52d59c8a51511956b272a3bb01e1daeb7
ajouatom <ajouatom@gmail.com>
2023-07-01 08:13:45 +0900

    fix....

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1532,6 +1532,10 @@ void DrawApilot::drawLeadApilot(const UIState* s) {
             case 2: ui_draw_image(s, { bx - icon_size / 2, by - icon_size / 2, icon_size, icon_size }, "ic_turn_r", 1.0f); break;
             case 3: ui_draw_image(s, { bx - icon_size / 2, by - icon_size / 2, icon_size, icon_size }, "ic_lane_change_l", 1.0f); break;
             case 4: ui_draw_image(s, { bx - icon_size / 2, by - icon_size / 2, icon_size, icon_size }, "ic_lane_change_r", 1.0f); break;
+            default:
+                sprintf(str, "%d", xTurnInfo);
+                ui_draw_text(s, bx, by + 20, str, 35, COLOR_BLACK, BOLD, 0.0f, 0.0f);
+                break;
             }
         }
         else if (roadLimitSpeed > 0 && roadLimitSpeed < 200) {
